ae65a8b171def00df8ef2c23e8e925bfacd6e2f6
Zhao Lin <linzhaolover@163.com>
2023-06-30 15:39:19 +0800

    add zip_dir

---
+++ zip/zip_dir.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+# function zip dir with password "654321"
+# $1: dir path
+# $2: zip file path
+# $3: password
+function zip_dir() {
+    if [ $# -lt 2 ]; then
+        echo "Usage: $0 dir_path zip_file_path [password]"
+        return 1
+    fi
+
+    dir_path=$1
+    zip_file_path=$2
+    password=$3
+
+    if [ -z "$password" ]; then
+        password="654321"
+    fi
+
+    if [ ! -d "$dir_path" ]; then
+        echo "dir_path: $dir_path is not a dir"
+        return 1
+    fi
+
+    if [ -f "$zip_file_path" ]; then
+        echo "zip_file_path: $zip_file_path is already exist"
+        return 1
+    fi
+
+    zip -r -P $password $zip_file_path $dir_path
+    return $?
+}
+
+# function unzip dir with password "654321"
+# $1: zip file path
+# $2: unzip dir path
+# $3: password
+function unzip_dir() {
+    if [ $# -lt 2 ]; then
+        echo "Usage: $0 zip_file_path unzip_dir_path [password]"
+        return 1
+    fi
+
+    zip_file_path=$1
+    unzip_dir_path=$2
+    password=$3
+
+    if [ -z "$password" ]; then
+        password="654321"
+    fi
+
+    if [ ! -f "$zip_file_path" ]; then
+        echo "zip_file_path: $zip_file_path is not a file"
+        return 1
+    fi
+
+    if [ -d "$unzip_dir_path" ]; then
+        echo "unzip_dir_path: $unzip_dir_path is already exist"
+        return 1
+    fi
+
+    unzip -P $password $zip_file_path -d $unzip_dir_path
+    return $?
+}
+
+# function check cmd for zip_dir or unzip_dir
+# $1: cmd
+function check_zip_or_unzip()
+{
+    cmd=$1
+    
+    if [ "X$cmd" == "Xzip"]; then
+        zip_dir $2 $3 $4
+        return $?
+    elif [ "X$cmd" == "Xunzip"]; then
+        unzip_dir $2 $3 $4
+        return $?
+    else
+        echo "Usage: $0 zip|unzip dir_path zip_file_path [password]"
+        return 1
+    fi
+}
+
+# main here
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 zip|unzip dir_path zip_file_path [password]"
+    exit 1
+fi
+check_zip_or_unzip $1 $2 $3 $4
+exit $?
