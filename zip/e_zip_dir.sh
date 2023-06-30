7c8291acbee89a39603ae38654fd0c485f035748
Zhao Lin <linzhaolover@163.com>
2023-06-30 16:00:45 +0800

    add e_zip_dir.sh

--- zip/e_zip_dir.sh
+++ zip/e_zip_dir.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
 
-# function zip dir with password "654321"
+# function zip dir with encrypt
 # $1: dir path
 # $2: zip file path
-# $3: password
 function zip_dir() {
     if [ $# -lt 2 ]; then
-        echo "Usage: $0 dir_path zip_file_path [password]"
+        echo "Usage: $0 dir_path zip_file_path encrypt"
         return 1
     fi
 
     dir_path=$1
     zip_file_path=$2
-    password=$3
-
-    if [ -z "$password" ]; then
-        password="654321"
-    fi
 
     if [ ! -d "$dir_path" ]; then
         echo "dir_path: $dir_path is not a dir"
@@ -28,27 +22,21 @@ function zip_dir() {
         return 1
     fi
 
-    zip -r -P $password $zip_file_path $dir_path
+    zip -r -e $zip_file_path $dir_path
     return $?
 }
 
-# function unzip dir with password "654321"
+# function unzip dir with encrypt
 # $1: zip file path
 # $2: unzip dir path
-# $3: password
 function unzip_dir() {
     if [ $# -lt 2 ]; then
-        echo "Usage: $0 zip_file_path unzip_dir_path [password]"
+        echo "Usage: $0 zip_file_path unzip_dir_path encrypt"
         return 1
     fi
 
     zip_file_path=$1
     unzip_dir_path=$2
-    password=$3
-
-    if [ -z "$password" ]; then
-        password="654321"
-    fi
 
     if [ ! -f "$zip_file_path" ]; then
         echo "zip_file_path: $zip_file_path is not a file"
@@ -60,7 +48,7 @@ function unzip_dir() {
         return 1
     fi
 
-    unzip -P $password $zip_file_path -d $unzip_dir_path
+    unzip -e $zip_file_path -d $unzip_dir_path
     return $?
 }
 
@@ -72,23 +60,23 @@ function check_zip_or_unzip()
     
     # set -x
     if [ "X$cmd" == "X-zip" ]; then
-        zip_dir $2 $3 $4
+        zip_dir $2 $3
         return $?
     elif [ "X$cmd" == "X-unzip" ]; then
-        unzip_dir $2 $3 $4
+        unzip_dir $2 $3
         return $?
     else
-        echo "Usage: $0 -zip dir_path zip_file_path [password]"
-        echo "Usage: $0 -unzip zip_file_path dir_path [password]"
+        echo "Usage: $0 -zip dir_path zip_file_path encrypt"
+        echo "Usage: $0 -unzip zip_file_path dir_path encrypt"
         return 1
     fi
 }
 
 # main here
 if [ $# -lt 3 ]; then
-    echo "Usage: $0 -zip dir_path zip_file_path [password]"
-    echo "Usage: $0 -unzip zip_file_path dir_path [password]"
+    echo "Usage: $0 -zip dir_path zip_file_path encrypt"
+    echo "Usage: $0 -unzip zip_file_path dir_path encrypt"
     exit 1
 fi
-check_zip_or_unzip $1 $2 $3 $4
+check_zip_or_unzip $1 $2 $3
 exit $?
